0efefc63a627d9eac1f436344c31445037730ced
Abdul Wahab <abdulwahab@gmail.com>
2024-04-04 11:34:44 +0500

    final touch done

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+
+    // for tablet smooth
+    tablet: { smooth: true },
+
+    // for mobile
+    smartphone: { smooth: true },
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+}
+
+function loadingAnimation() {
+  var tl = gsap.timeline();
+  tl.from("#page1", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 0.2,
+  });
+  tl.from("#page1", {
+    transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+    borderRadius: "150px",
+    duration: 2,
+    ease: "expo.out",
+  });
+  tl.from("nav", {
+    opacity: 0,
+    delay: -0.2,
+  });
+  tl.from("#page1 h1, #page1 p, #page1 div", {
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.2,
+  });
+}
+
 function navAnimation() {
   var nav = document.querySelector("nav");
 
@@ -100,7 +160,6 @@ function page6Animations() {
     scrollTrigger: {
       trigger: "#btm6-part2",
       scroller: "#main",
-      // markers:true,
       start: "top 80%",
       end: "top 10%",
       scrub: true,
